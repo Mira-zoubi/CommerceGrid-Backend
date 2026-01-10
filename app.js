b26584eb551eb637f524edfb08866bcfdeeb23f7
mirazoubi-maker <mirazoubi2022@outlook.com>
2026-01-10 21:19:54 +0300

    Add Aramex products with schema, seed, and API endpoint

---
+++ app.js
@@ -0,0 +1,27 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import AramexProductRoutes from "./routes/AramexProductRoutes.js";
+import cors from "cors";
+
+ 
+dotenv.config();
+const app=express();
+const PORT=process.env.PORT;
+
+ // ✅ ENABLE CORS
+app.use(cors({
+  origin: "http://localhost:5179"
+}));
+app.use("/uploads", express.static("uploads"));
+app.use("/api/aramex-products", AramexProductRoutes);
+
+mongoose.connect(process.env.CONN)
+.then((result)=>{
+    app.listen(PORT, ()=>{
+        console.log("listening successfully");
+    })
+})
+.catch((err)=>{
+    console.log(err)
+})
